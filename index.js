4f9633af47b02ba687e98da72574256f8367fdee
404pnf <404pnf@exmaple.org>
2015-12-31 09:43:33 +0800

    换成了js-standard和jshint的代码风格；修复错误，object literal中不能用this

--- index.js
+++ index.js
@@ -1,46 +1,45 @@
 /**
  * fis.baidu.com
  */
-
-/*global
-    fis
- */
-
-/*jslint
-    node: true, stupid: true, nomen: true
- */
-
-'use strict';
-var _ = fis.util, // TODO 这个没有用到啊
-    assert = require('assert'),
-    path = require('path'),
-    fs = require('fs'),
-    child_process = require('child_process'),
-    crypto = require('crypto'),
-    str = require('string');
+/* jslint
+ vars: true, indent: 2, stupid: true, node: true
+*/
+/* jshint asi: true, node: true */
+'use strict'
+var assert = require('assert')
+var path = require('path')
+var child_process = require('child_process')
+var crypto = require('crypto')
+var str = require('string')
 module.exports = function (options) {
-    assert(options.to, 'options.to is required!');
-    assert(options.source, 'options.source is required!');
-    var opt = {
-            to: options.to,
-            source: options.source,
-            server: options.server,
-            dir: path.join(process.cwd(), this.source),
-            tmpFilename: crypto.randomBytes(36).toString('hex') + '.zip', // 避免本来目录已经有个文件叫做 dist.zip
-            local_zipfile: path.join(this.source, this.tmpFilename),
-        },
-        cmd = str('cd {dir}; zip -r {tmpFilename} ./*').template(opt).s,
-        scp_cmd = str('scp {opt.local_zipfile} server:{opt.to}').template(opt).s,
-        unzip_cmd = str('ssh {opt.server} "cd {opt.to}; unzip -o {opt.tmpFilename}; rm {opt.tmpFilename}; exit"').template(opt).s,
-        remove_local_zip = str('rm {opt.tmpFilename}').template(opt).s,
-        commands = [
-            [cmd, '|||-> 本地打包完成'],
-            [scp_cmd, '|||-> 上传完成'],
-            [unzip_cmd, '|||-> 远程部署完成'],
-            [remove_local_zip, '|||-> 本地清理完成']
-        ];
-    commands.each(function (e) {
-        child_process.execSync(e[0]);
-        console.log(e[1]);
-    });
-};
+  assert(options.to, 'options.to is required!')
+  assert(options.source, 'options.source is required!')
+  var to = options.to
+  var source = options.source
+  var server = options.server
+  var dir = path.join(process.cwd(), source)
+  var tmpFilename = crypto.randomBytes(36).toString('hex') + '.zip'
+  var local_zipfile = path.join(source, tmpFilename)
+  var opt = {
+    to: to,
+    source: source,
+    server: server,
+    dir: dir,
+    tmpFilename: tmpFilename,
+    local_zipfile: local_zipfile
+  }
+  var cmd = str('cd {opt.dir}; zip -r {opt.tmpFilename} ./*').template(opt).s
+  var scp_cmd = str('scp {opt.local_zipfile} server:{opt.to}').template(opt).s
+  var unzip_cmd = str('ssh {opt.server} "cd {opt.to}; unzip -o {opt.tmpFilename}; rm {opt.tmpFilename}; exit"').template(opt).s
+  var remove_local_zip = str('rm {opt.tmpFilename}').template(opt).s
+  var commands = [
+    [cmd, '|||-> 本地打包完成'],
+    [scp_cmd, '|||-> 上传完成'],
+    [unzip_cmd, '|||-> 远程部署完成'],
+    [remove_local_zip, '|||-> 本地清理完成']
+  ]
+  commands.each(function (e) {
+    child_process.execSync(e[0])
+    console.log(e[1])
+  })
+}
